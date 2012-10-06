726fa560e333a9a51eda8c4909680ed4e28a7a2e
Jonas Gebhardt <jonas.gebhardt@gmail.com>
2012-10-05 21:32:09 -0400

    forcepad->colorpicker via websockets

--- colorpicker/js/testingBot.js
+++ colorpicker/js/testingBot.js
@@ -99,3 +99,118 @@ window.addEventListener('keydown', function(event) {
     	break;
   	}
 }, false);
+
+var c = 0
+var fhist = []
+var histCount = 20
+
+//converts ranges to 0..100 
+var setColorByPad = function(data) {
+  
+  //x, y, f: raw force, s: touch size, f2: filtered force
+  var x, y, f, s, f2 = 0
+  var conversions = {
+    x: {min: 1000, max: 5800, target: 100},
+    y: {min: 1200, max: 4700, target: 100},
+    f: {min: 0, max: 500, target: 100},
+    s: {min: 0, max: 100, target: 100},
+    f2: {min: 0, max: 500, target: 100}
+  }
+  
+
+  if (data || data.f || data.f[0]) {
+    x = (data.f[0][0] - conversions.x.min) / (conversions.x.max - conversions.x.min) * conversions.x.target
+    y = (conversions.y.max - data.f[0][1]) / (conversions.y.max - conversions.y.min) * conversions.y.target
+    f = Math.max(data.f[0][2],conversions.f.min) / (conversions.f.max - conversions.f.min) * conversions.f.target
+    s = Math.max(data.f[0][3],conversions.s.min) / (conversions.s.max - conversions.s.min) * conversions.s.target
+    f2 = Math.max(data.f[0][4],conversions.f.min) / (conversions.f2.max - conversions.f2.min) * conversions.f2.target
+    
+    c++
+    fhist[c % histCount] = f
+    var favg = _.reduce(fhist, function(i, m){return m+i})/histCount
+    setColor(x, y, favg)
+    //console.log(fhist, favg, f)
+  } else {
+    console.log('empty packet: ', data)
+
+  }
+}
+
+var bounds = {
+    c: 0,
+    x: {min: 0, max: 0, sum:0, avg: 0},
+    y: {min: 0, max: 0, sum:0, avg: 0},
+    f: {min: 0, max: 0, sum:0, avg: 0},
+    s: {min: 0, max: 0, sum:0, avg: 0},
+    f2: {min: 0, max: 0, sum:0, avg: 0}
+}
+
+var findBounds = function(data){
+  //console.log(data)
+  var old = bounds;
+  bounds.c++
+  changed = false
+  if(data[0][0] < bounds.x.min) { bounds.x.min = data[0][0]; changed = true;}
+  if(data[0][1] < bounds.y.min) { bounds.y.min = data[0][1]; changed = true;}
+  if(data[0][2] < bounds.f.min) { bounds.f.min = data[0][2]; changed = true;}
+  if(data[0][3] < bounds.s.min) { bounds.s.min = data[0][3]; changed = true;}
+  if(data[0][4] < bounds.f2.min) { bounds.f2.min = data[0][4]; changed = true;}
+  
+  if(data[0][0] > bounds.x.max) { bounds.x.max = data[0][0]; changed = true;}
+  if(data[0][1] > bounds.y.max) { bounds.y.max = data[0][1]; changed = true;}
+  if(data[0][2] > bounds.f.max) { bounds.f.max = data[0][2]; changed = true;}
+  if(data[0][3] > bounds.s.max) { bounds.s.max = data[0][3]; changed = true;}
+  if(data[0][4] > bounds.f2.max) { bounds.f2.max = data[0][4]; changed = true;}
+
+  bounds.x.sum += data[0][0]
+  bounds.y.sum += data[0][1]
+  bounds.f.sum += data[0][2]
+  bounds.s.sum += data[0][3]
+  bounds.f2.sum += data[0][4]
+
+  bounds.x.sum = bounds.x.sum / bounds.c
+  bounds.y.avg = bounds.y.sum / bounds.c
+  bounds.f.avg = bounds.f.sum / bounds.c
+  bounds.s.avg = bounds.s.sum / bounds.c
+  bounds.f2.avg = bounds.f2.sum / bounds.c
+
+  if (changed) {
+    console.log(bounds)
+  }
+}
+
+
+var FakeWebSocket = function() {
+  
+    
+    //setTimeout(trigger, 500)
+    onopen()
+    
+    var trigger = function() {
+      
+      this.onMessage(generateOutput())
+      setTimeout(trigger, 500)  
+    }
+
+    var generateOutput = function(){
+      return '{"fc":1, "f":[[2834,2650,-2,52,0.343923]]}'
+    }
+  
+};
+
+$(document).ready(function() {
+
+  var ws = new WebSocket("ws://128.237.128.231:9999/");
+  ws.onopen = function() {
+    console.log('onOpen');
+    ws.send("Hello Mr. Server!");
+    
+  };
+  ws.onmessage = function (e) {
+    var data = $.parseJSON(e.data)
+    findBounds(data.f)
+    setColorByPad(data);
+  };
+  ws.onclose = function() { };
+
+});
